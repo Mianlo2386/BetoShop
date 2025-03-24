0f412bc4d18f133b1a791a9ef649ba26f9713f7a
Miguel Lopez <miguellopez32@gmail.com>
2025-03-24 12:47:22 -0300

    Comienzo de union de front con bakend

---
+++ src/main/java/com/mianlodev/BetoStore/BetoStoreApplication.java
@@ -0,0 +1,13 @@
+package com.mianlodev.BetoStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetoStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BetoStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mianlodev/BetoStore/BetoStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mianlodev.BetoStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetoStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
